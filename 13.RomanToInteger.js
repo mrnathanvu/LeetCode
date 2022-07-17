fa0013daf122731b54b68036a9376a060af2ac2f
Nathan Vu <mr.nathanvu@gmail.com>
2022-07-16 20:43:46 -0700

    Solve 13.RomanToInteger.js

--- 13.RomanToInteger.js
+++ 13.RomanToInteger.js
@@ -54,4 +54,63 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 // Example 3:
 // s = "MCMXCIV";
 
-var romanToInt = function (s) {};
+var romanToInt = function (s) {
+    // Define Roman numerals with an array of symbols
+    const symbol = ["I", "V", "X", "L", "C", "D", "M"];
+    // Define Roman numerals with an array of values
+    const value = [1, 5, 10, 50, 100, 500, 1000];
+
+    // An array of all evalutated values
+    let evalutatedValue = [];
+    // Result to return
+    let result = 0;
+
+    // Use a for loop to iterates through the elements in the parameter
+    for (let i = 0; i < s.length; i++) {
+        // console.log("Element: ", s[i]);
+        // Use a for loop to iterates through symbol array
+        for (let j = 0; j < symbol.length; j++) {
+            // If the parameter element matches the symbol element
+            if (s[i] === symbol[j]) {
+                // console.log("Symbol: ", symbol[j]);
+                // Use the current index to retrive the corresponding value from the value array
+                let currentValue = value[j];
+                // console.log("Value: ", currentValue);
+                // Push the value to evalutatedValue array
+                evalutatedValue.push(currentValue);
+                // console.log("..........");
+            }
+        }
+    }
+    // console.log("evalutatedValue: ", evalutatedValue);
+
+    // console.log("==========");
+
+    // Use a for loop to iterates through evalutatedValue array
+    for (let k = 0; k < evalutatedValue.length; k++) {
+        let currentValue = evalutatedValue[k];
+        let nextValue = evalutatedValue[k + 1];
+        // If the current value is smaller than next value
+        if (currentValue < nextValue) {
+            // console.log("Current value: ", currentValue);
+            // console.log("Next value: ", nextValue);
+            // Subtract the current evalutatedValue element from the next evalutatedValue element
+            let difference = nextValue - currentValue;
+            // console.log("The difference: ", difference);
+            evalutatedValue.splice(k, 2, difference);
+            // console.log("..........");
+        }
+    }
+    // console.log("evalutatedValue: ", evalutatedValue);
+
+    // Use a for loop to iterates through evalutatedValue array
+    for (let l = 0; l < evalutatedValue.length; l++) {
+        // Accumulate all the values from evalutatedValue array and store in result
+        let currentValue = evalutatedValue[l];
+        result += currentValue;
+    }
+    // console.log("evalutatedValue: ", evalutatedValue);
+    return result;
+};
+
+console.log(romanToInt(s));
